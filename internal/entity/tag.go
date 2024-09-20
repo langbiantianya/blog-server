4f0df7a00c024c28730adb5fe97922b8aa9ebd38
快醒醒你不是条单身汪 <langbiantianya@outlook.com>
2024-09-20 03:34:47 +0000

    fix: entity序列化

--- internal/entity/tag.go
+++ internal/entity/tag.go
@@ -6,6 +6,6 @@ type Tag struct {
 	ID        uint      `json:"id,omitempty" gorm:"primarykey"`
 	CreatedAt time.Time `json:"createdAt,omitempty" `
 	UpdatedAt time.Time `json:"updatedAt,omitempty" `
-	Name      string    `json:"name" gorm:"type:varchar(128);not null; unique;"`
-	Essays    []Essay   `gorm:"many2many:essay_tags;"`
+	Name      string    `json:"name,omitempty" gorm:"type:varchar(128);not null; unique;"`
+	Essays    []Essay   `json:"essays,omitempty" gorm:"many2many:essay_tags;"`
 }
