8bbdfe60f75a7b5a3ec12de6a08e0ef1be2f2012
快醒醒你不是条单身汪 <langbiantianya@outlook.com>
2024-09-20 03:27:03 +0000

    fix: entity序列化

--- internal/entity/essay.go
+++ internal/entity/essay.go
@@ -3,11 +3,11 @@ package entity
 import "time"
 
 type Essay struct {
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Title     string `json:"title,omitempty" gorm:"type:varchar(256);not null;"`
-	Post      string `json:"post,omitempty" gorm:"type:text;not null;"`
-	Hide      bool   `json:"hide,omitempty" gorm:"type:tinyint(1);not null;default:false;"`
-	Tags      []Tag  `json:"tags,omitempty" gorm:"many2many:essay_tags;"`
+	ID        uint      `json:"id,omitempty" gorm:"primarykey"`
+	CreatedAt time.Time `json:"createdAt,omitempty" `
+	UpdatedAt time.Time `json:"updatedAt,omitempty" `
+	Title     string    `json:"title,omitempty" gorm:"type:varchar(256);not null;"`
+	Post      string    `json:"post,omitempty" gorm:"type:text;not null;"`
+	Hide      bool      `json:"hide,omitempty" gorm:"type:tinyint(1);not null;default:false;"`
+	Tags      []Tag     `json:"tags,omitempty" gorm:"many2many:essay_tags;"`
 }
--- internal/entity/tag.go
+++ internal/entity/tag.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Tag struct {
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Name      string   `json:"name" gorm:"type:varchar(128);not null; unique;"`
-	Essays    []Essay `gorm:"many2many:essay_tags;"`
+	ID        uint      `json:"id,omitempty" gorm:"primarykey"`
+	CreatedAt time.Time `json:"createdAt,omitempty" `
+	UpdatedAt time.Time `json:"updatedAt,omitempty" `
+	Name      string    `json:"name" gorm:"type:varchar(128);not null; unique;"`
+	Essays    []Essay   `gorm:"many2many:essay_tags;"`
 }
