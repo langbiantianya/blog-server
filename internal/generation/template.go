e9151c355be32dcb00e317ad45a8fa80bb811efa
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2024-10-22 17:12:00 +0800

    feat: 替换模板为go template

--- internal/generation/template.go
+++ internal/generation/template.go
@@ -4,56 +4,8 @@ import (
 	"blog-server/internal/entity"
 	"blog-server/public/utils"
 	"bytes"
-	"io"
-	"os"
-	"strings"
 	"text/template"
 )
-
-func ApplayTemplate(postTemplatePath, tagTemplatePath, title string, tag []string, postHtmlStr string) (string, error) {
-	// 加载模板
-	postTemplateFile, err := os.Open(postTemplatePath)
-
-	if err != nil {
-		return "", err
-	}
-	defer postTemplateFile.Close()
-
-	postTemplateByteArray, err := io.ReadAll(postTemplateFile)
-	if err != nil {
-		return "", err
-	}
-
-	tagTemplateFile, err := os.Open(tagTemplatePath)
-
-	if err != nil {
-		return "", err
-	}
-	defer tagTemplateFile.Close()
-
-	tagTemplateByteArray, err := io.ReadAll(tagTemplateFile)
-
-	if err != nil {
-		return "", err
-	}
-	// 生成tag
-	tags := utils.Map(tag, func(index int, t string) (string, error) {
-		return strings.ReplaceAll(string(tagTemplateByteArray), "${{tag}}", t), nil
-	})
-
-	tagStr := utils.Sum(tags, func(sum string, item string) string {
-		return sum + item
-	})
-
-	// 替换title
-	htmlStr := strings.ReplaceAll(string(postTemplateByteArray), "${{title}}", title)
-
-	htmlStr = strings.ReplaceAll(htmlStr, "${{tag}}", tagStr)
-
-	// essay
-	htmlStr = strings.ReplaceAll(htmlStr, "${{post}}", postHtmlStr)
-	return htmlStr, nil
-}
 
 func GenerationPostV2(postTemplatePath string, essay entity.Essay) (string, error) {
 	postTemplate, err := template.ParseFiles(postTemplatePath)
